1066e5ad9954e749b3e2c70475551029b1324872
Artem Bilousov <artem.bilousov.1993@gmail.com>
2023-12-11 19:24:14 +0100

    add class Record

--- contacts_book.py
+++ contacts_book.py
@@ -21,3 +21,25 @@ class Phone(Field):
             raise ValueError("Phone must be a 10-digit numeric value.")
         
 
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        new_phone = Phone(phone)
+        self.phones.append(new_phone)
+
+    def edit_phone(self, old_phone, new_phone):
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p.value
+        raise ValueError("Phone is not found.")
+                         
+    def __str__(self):
+        phones_str = ': '.join(str(p) for p in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones_str}"
